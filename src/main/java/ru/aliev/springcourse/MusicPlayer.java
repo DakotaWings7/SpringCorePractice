bf43970e9236452266469097ab5a367586303b68
Djanik Aliyev <djanik_aliyev@mail.ru>
2024-04-03 23:53:03 +0300

    Dependency Injection from XML applicationContext file by using Setter method and <property name=. ref=./> tag

--- src/main/java/ru/aliev/springcourse/MusicPlayer.java
+++ src/main/java/ru/aliev/springcourse/MusicPlayer.java
@@ -3,7 +3,14 @@ package ru.aliev.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
