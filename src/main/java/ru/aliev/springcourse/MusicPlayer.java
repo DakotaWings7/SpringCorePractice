cd92bfb3e421edfc2098d4feb8cf52c1820b3e9e
Djanik Aliyev <djanik_aliyev@mail.ru>
2024-04-04 23:02:08 +0300

    First using @Qualifier annotation. Useful to specify which bean need to inject

--- src/main/java/ru/aliev/springcourse/MusicPlayer.java
+++ src/main/java/ru/aliev/springcourse/MusicPlayer.java
@@ -1,21 +1,16 @@
 package ru.aliev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    @Qualifier("rockMusic")
+    private Music music;
 
     public void play() {
-        System.out.println("Playing: " + classicalMusic.getSong());
-        System.out.println("Playing: " + rockMusic.getSong());
+        System.out.println("Playing: " + music.getSong());
     }
 }
