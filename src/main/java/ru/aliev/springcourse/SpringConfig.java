16024a9d11bdfb34bfc7d516715e690040579e0f
Djanik Aliyev <djanik_aliyev@mail.ru>
2024-04-05 17:29:59 +0300

    Now all configuration is in Java-code. Used @Configuration to mark config file, @PropertySource and @ComponentScan. Set SpringConfig using AnnotationConfigApplicationContext class

---
+++ src/main/java/ru/aliev/springcourse/SpringConfig.java
@@ -0,0 +1,12 @@
+package ru.aliev.springcourse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.aliev.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+
+}
